b7ff610f8a8bcba71233c8544f918f12b3a9edef
Jesus Manny Baez <jesusmbaez@gmail.com>
2018-08-31 06:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Manny"
-var height = "3"
+var height = 70
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
